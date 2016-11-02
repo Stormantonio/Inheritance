584da3d840f75a9c62c6bad7e793e05c8e32f036
Stormantonio <stormantonio.che@gmail.com>
2016-11-02 23:37:26 +0200

    lesson Inheritance

---
+++ src/com/company/Loader.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by Anton on 02.11.2016.
+ */
+public class Loader {
+
+    public static void main(String[] args) {
+        Dog tuzik = new Dog();
+        Cat murzik = new Cat();
+        tuzik.size();
+        tuzik.color();
+        tuzik.sex();
+        tuzik.weight();
+        murzik.size();
+        murzik.color();
+        murzik.sex();
+        murzik.weight();
+    }
+}
